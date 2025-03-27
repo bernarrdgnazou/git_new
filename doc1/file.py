5ed6ae8ba65a129c64589dda2459273628a5f669
bern <74977843+bernarrdgnazou@users.noreply.github.com>
2025-03-27 19:48:15 +0000

    Update file.py
    ajout d'une voiture

--- doc1/file.py
+++ doc1/file.py
@@ -1,1 +1,17 @@
 print("vous avez {age} ans".format(age = 2025 - int(input("entrez votre année de naissance: >>>"))))
+
+
+class Voiture:
+  def __init__(self, nb_pneu, marque, model, couleur):
+    self.nb_pneu = nb_pneu
+    self.marque = marque
+    self.model = model
+    self.couleur = couleur
+
+  def demarrer(self):
+    print(f"la voiture de marque {self.marque}, de model {self.model} et de couleur {self.couleur} a demarrer")
+
+
+
+ma_voiture = Voiture(8, "Pkandji", "Djetran", "noire")
+ma_voiture.demarrer()
